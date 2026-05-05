1275a0cc3fd36e79f57eb0859fa166d2485be531
Adrian Valdes <adriangvaldes@gmail.com>
2026-05-04 21:59:16 -0300

    refactor: update server configuration and add environment handling
    - Removed .vscode/settings.json
    - Updated dev script in package.json to include env-file
    - Enhanced server error handling in server.ts
    - Configured server to use environment variables for port and database URL
    - Added docker-compose.yml for PostgreSQL service
    - Created init.sql for database initialization
    - Introduced env.ts for environment variable validation

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,36 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from '@/env'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  console.error(error)
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
+
 server.register(fastifyCors, { origin: '*' })
 
-server.listen({ port: 3333, host: 'localhost' }).then(() => {
-  console.log('Server is running on http://localhost:3333')
+console.log(env.DATABASE_URL)
+
+server.listen({ port: env.PORT, host: 'localhost' }).then(() => {
+  console.log('Server is running on http://localhost:' + env.PORT)
 })
