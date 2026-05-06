d1391ced31579690d3ce5d12d184f0006834eccf
Adrian Valdes <adriangvaldes@gmail.com>
2026-05-06 01:55:05 -0300

    feat(routes): handle image upload flow

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,7 +1,7 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
-import { db } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
+import { uploadImage } from '@/app/functions/upload-image'
+import { isRight, unwrapEither } from '@/shared/either'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
   server.post(
@@ -11,7 +11,8 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         summary: 'Upload an image',
         consumes: ['multipart/form-data'],
         response: {
-          201: z.object({ uploadId: z.string() }),
+          201: z.null().describe('Image uploaded successfully'),
+          400: z.object({ message: z.string() }),
           409: z
             .object({ message: z.string() })
             .describe('Conflict - Image already exists'),
@@ -21,17 +22,37 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
     async (request, reply) => {
       const uploadedFile = await request.file({
         limits: {
-          fileSize: 5 * 1024 * 1024, // 5MB
+          fileSize: 2 * 1024 * 1024, // 2MB
         },
       })
 
-      await db.insert(schema.uploads).values({
-        name: 'teste.jpg',
-        remoteKey: 'teste-remote-key',
-        remoteUrl: 'https://example.com/teste.jpg',
+      if (!uploadedFile) {
+        return reply.status(400).send({ message: 'File is required' })
+      }
+
+      const result = await uploadImage({
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
       })
 
-      return reply.status(201).send({ uploadId: 'teste123' })
+      if (uploadedFile.file.truncated) {
+        return reply
+          .status(400)
+          .send({ message: 'File size exceeds the limit of 2MB' })
+      }
+
+      if (isRight(result)) {
+        console.log(unwrapEither(result))
+        return reply.status(201).send(null)
+      }
+
+      const error = unwrapEither(result)
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFormat':
+          return reply.status(400).send({ message: error.message })
+      }
     }
   )
 }
