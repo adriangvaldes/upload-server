e17542f816c35d876554efbfc5beb912d3675659
Adrian Valdes <adriangvaldes@gmail.com>
2026-05-12 00:14:40 -0300

    feat(tests): add unit tests for get uploads functionality feat(factory): create makeUpload helper function for test uploads feat(dependencies): add dayjs and @faker-js/faker to devDependencies

---
+++ src/test/factory/make-upload.ts
@@ -0,0 +1,21 @@
+import { fakerPT_BR as faker } from '@faker-js/faker'
+import { InferInsertModel } from 'drizzle-orm'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+
+export async function makeUpload(
+  overrides?: Partial<InferInsertModel<typeof schema.uploads>>
+) {
+  const fileName = faker.system.fileName()
+  const result = await db
+    .insert(schema.uploads)
+    .values({
+      name: fileName,
+      remoteKey: `images/${fileName}`,
+      remoteUrl: faker.internet.url(),
+      ...overrides,
+    })
+    .returning()
+
+  return result[0]
+}
